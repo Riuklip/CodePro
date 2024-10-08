63319b93d273b1e8068ee30270cf5d5eacefa332
Riuklip <143903240+Riuklip@users.noreply.github.com>
2024-10-08 16:52:37 -0400

    actualizacion del scroll
    modificaciones en css y js para lograr pausar el scroll al ver menu hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,30 @@
 export default function hamburgerMenu(e,menu,boton){
     const d=document;    
+   
+    
     if(e.target.matches(boton) || e.target.matches(".menu_button *")){
         d.querySelector(menu).classList.toggle("is-active");
         d.querySelector(boton).classList.toggle("is-active");
+        console.log(d.querySelector(boton).classList);
+        let botonClass= d.querySelector(boton).classList
+        console.log(botonClass.contains("is-active"))
+         if (botonClass.contains("is-active")){
+            disableScroll()
+         }else{
+             
+            enableScroll()
+            };
+         }
         
+         function disableScroll() {
+            d.body.classList.add("desactiva_scroll");
+            console.log("Scroll desactivado")
+        }
         
+        function enableScroll() {
+            d.body.classList.remove("desactiva_scroll");
+          
+        }
     }
-    
-}
+
 
